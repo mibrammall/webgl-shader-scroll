03f3de98eb47cd324f6b0b4fe57d55f4a5bff6b1
M Brammall <mibrammall.github@gmail.com>
2023-02-18 20:03:06 +0000

    Basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import { ReactElement } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
+const sections = ["Logma", "Naos", "Chara"];
+
+function App(): ReactElement {
+  return (
+    <>
+      {sections.map((title, index) => (
+        <Section section={title} index={index} />
+      ))}
+    </>
+  );
+}
+
+interface SectionProps {
+  section: string;
+  index: number;
+}
+
+function Section({ section, index }: SectionProps): ReactElement<SectionProps> {
+  return (
+    <>
+      <div className="section__title">
+        <h1 className="section__title-number">{index}</h1>
+        <h2 className="section__title-title">{section}</h2>
+      </div>
+    </>
+  );
 }
 
 export default App;
