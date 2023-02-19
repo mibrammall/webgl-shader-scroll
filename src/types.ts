9f49cee74b0831a4983f67c6b5257de7f306bb13
M Brammall <mibrammall.github@gmail.com>
2023-02-19 20:32:00 +0000

    Some common types

---
+++ src/types.ts
@@ -0,0 +1,14 @@
+export interface ViewPort {
+  width: number;
+  height: number;
+}
+
+export interface Scroll {
+  ease: number;
+  hard: number;
+  height: number;
+  limit: number;
+  normalized: number;
+  running: boolean;
+  soft: number;
+}
